1bf7b55851cd72879ad5b2145f2bf0f74d125a16
Andriy Duyko <aduyko@gmail.com>
2015-06-28 20:00:10 -0400

    decided to generate horoscopes locally, started progress on that

--- chrome_extension/options/options.js
+++ chrome_extension/options/options.js
@@ -1,19 +1,27 @@
+function validateBirthday(birthday) {
+
+}
+
 // save inputted name and birthday
-function save_options() {
+function saveOptions() {
     var name = document.getElementById('name').value;
     var birthday = document.getElementById('birthday').value;
-    chrome.storage.sync.set({
-        name: name,
-        birthday: birthday
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('update_status');
-        status.innerHTML = 'Settings saved';
-    });
+    var statusMessage = "Settings saved";
+    if (validateBirthday(birthday)) {
+        chrome.storage.sync.set({
+            name: name,
+            birthday: birthday
+        }, function() {
+            // Update status to let user know options were saved.
+            
+        });
+    } 
+    var status = document.getElementById('update_status');
+    status.innerHTML = 'Settings saved';
 }
 
 // deletes a users saved name and birthday, clears inputs
-function clear_options() {
+function clearOptions() {
     chrome.storage.sync.set({
         name: '',
         birthday: ''
@@ -27,29 +35,29 @@ function clear_options() {
 }
 
 // restores a users name and birthday to populate inputs
-function restore_options() {
+function restoreOptions() {
     chrome.storage.sync.get({
         name: '',
         birthday: ''
-    }, function(user) {
-        document.getElementById('name').value = user.name;
-        document.getElementById('birthday').value = user.birthday;
+    }, function(data) {
+        document.getElementById('name').value = data.name;
+        document.getElementById('birthday').value = data.birthday;
     });
 }
 
 // startup, event listeners
 document.addEventListener('DOMContentLoaded', function() {
     // load saved name/birthday
-    restore_options();
+    restoreOptions();
     // save name/birthday
     document.getElementById('update_btn').addEventListener('click', function(e){
         e.preventDefault();
-        save_options();
+        saveOptions();
     });
     // clear name/birthday
     document.getElementById('clear_btn').addEventListener('click', function(e){
         e.preventDefault();
-        clear_options();
+        clearOptions();
     });
     // return to horoscope
     document.getElementById('return_link').addEventListener('click', function(e){
